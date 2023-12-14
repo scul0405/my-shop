07cd2b1ee8d54c7fe79c69fdbd584e4f6e413a1c
21120530 <phucmapcaumieu@gmail.com>
2023-12-14 09:51:51 +0700

    add client

---
+++ client/client/client/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace client
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
