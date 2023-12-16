47ed6385c31f2d90b265c2f4d80a50aa466dce89
21120530 <phucmapcaumieu@gmail.com>
2023-12-16 13:31:37 +0700

    [fuc/client] add Entity

---
+++ client/Entity/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    internal class Order
+    {
+        public Order() { }
+
+        public int Id { get; set; }
+        public List<Book> books { get; set; }
+        public DateTime created_at { get; set; }
+        public bool status { get; set; }
+        public int total { get; set; }
+    }
+}
