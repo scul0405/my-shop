da2e1dff3f3a7a8ab2729a48d55697bf9edf7b50
scul <90521402+scul0405@users.noreply.github.com>
2023-12-29 16:33:48 +0700

    [scul] minor fix: remove ')'

--- server/migrations/02_update_inital_tables.up.sql
+++ server/migrations/02_update_inital_tables.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE book_order ADD COLUMN quantity int not null default 1 check ( quantity > 0 ) );
+ALTER TABLE book_order ADD COLUMN quantity int not null default 1 check ( quantity > 0 );
 
 ALTER TABLE books DROP COLUMN sku;
 ALTER TABLE books DROP COLUMN image;
