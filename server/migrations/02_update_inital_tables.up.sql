0e0d265442434f1b3c056572ac169bdfbc53eb33
21120353 <90521402+scul0405@users.noreply.github.com>
2024-01-05 21:14:16 +0700

    [scul] Final update server files

--- server/migrations/02_update_inital_tables.up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE book_order ADD COLUMN quantity int not null default 1 check ( quantity > 0 );
-
-ALTER TABLE books DROP COLUMN sku;
-ALTER TABLE books DROP COLUMN image;
-
-ALTER TABLE books DROP CONSTRAINT books_category_id_fkey;
